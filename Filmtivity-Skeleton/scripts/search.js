8df2e02f53b5f716bd9c0015b97cbf959ac6aebc
Mariyan Yordanov <marsrewq@gmail.com>
2024-10-29 14:00:59 +0200

    fix bugs

--- Filmtivity-Skeleton/scripts/search.js
+++ Filmtivity-Skeleton/scripts/search.js
@@ -1,4 +1,3 @@
-let BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&`;
 let API_KEY = '2663a47da530c754207396eef5db5fb1';
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -14,15 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function fetchMovie(title) {
-    fetch(`${BASE_URL}t=${title}`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`)
         .then(response => response.json())
         .then(data => {
             const movie = data.results[0];
+            console.log(movie);
             document.getElementById('title').textContent = movie.title;
             document.getElementById('overview').textContent = movie.overview;
-            document.getElementById('release-date').textContent = movie.release_date;
-            document.getElementById('vote-average').textContent = movie.vote_average;
-            document.getElementById('original-language').textContent = movie.original_language;
+            document.getElementById('release_date').textContent = movie.release_date;
+            document.getElementById('vote_average').textContent = movie.vote_average;
+            document.getElementById('original_language').textContent = movie.original_language;
             document.getElementById('poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
         })
         .catch(error => console.error(error));
