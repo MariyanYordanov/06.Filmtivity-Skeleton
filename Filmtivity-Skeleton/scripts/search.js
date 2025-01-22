80898c515cde98ae272d67abe9cc999b655b8677
Mariyan Yordanov <marsrewq@gmail.com>
2025-01-22 19:52:57 +0200

    bug fix

--- Filmtivity-Skeleton/scripts/search.js
+++ Filmtivity-Skeleton/scripts/search.js
@@ -1,13 +1,16 @@
-let API_KEY = '2663a47da530c754207396eef5db5fb1';
+const API_KEY = '25afacdd7d9acf12478bb0c74e5d129a';
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('search-btn').addEventListener('click', function () {
-        const title = document.getElementById('search').value;
+document.addEventListener(`DOMContentLoaded`, function () {
+    document.getElementById(`search-btn`).addEventListener(`click`, function (e) {
+        e.preventDefault();
+        const title = document.getElementById(`search`).value;
         if (title.trim()) {
             fetchMovie(title);
-            document.getElementById('details-page').style.display = 'block';
-        } else {
-            document.getElementById('details-page').style.display = 'none';
+            document.getElementById(`details-page`).style.display = `block`;
+        }
+        else {
+            window.alert('something for test');
+            document.getElementById(`details-page`).style.display = `none`;
         }
     });
 });
@@ -16,14 +19,13 @@ function fetchMovie(title) {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`)
         .then(response => response.json())
         .then(data => {
-            const movie = data.results[0];
-            console.log(movie);
-            document.getElementById('title').textContent = movie.title;
-            document.getElementById('overview').textContent = movie.overview;
-            document.getElementById('release_date').textContent = movie.release_date;
-            document.getElementById('vote_average').textContent = movie.vote_average;
-            document.getElementById('original_language').textContent = movie.original_language;
-            document.getElementById('poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+            const results = data.results[0];
+            document.getElementById(`title`).textContent = results.title;
+            document.getElementById(`release_date`).textContent = results.release_date;
+            document.getElementById(`overview`).textContent = results.overview;
+            document.getElementById(`vote_average`).textContent = results.vote_average;
+            document.getElementById(`poster`).src = `https://image.tmdb.org/t/p/w500` + results.poster_path;
         })
         .catch(error => console.error(error));
 }
+
