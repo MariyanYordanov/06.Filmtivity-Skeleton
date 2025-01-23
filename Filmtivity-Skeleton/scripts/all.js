7fd2735bbe45cb141ad5885ae322b83b413c1402
Mariyan Yordanov <marsrewq@gmail.com>
2025-01-23 19:26:15 +0200

    fix bugs

--- Filmtivity-Skeleton/scripts/all.js
+++ Filmtivity-Skeleton/scripts/all.js
@@ -8,9 +8,7 @@ function fetchTopTenMovies() {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`)
         .then(response => response.json())
         .then(data => {
-            const results = data.results;
-            console.log(results.join(" "));
-            displayMovies(results);
+            displayMovies(data.results.slice(0, 12));
         })
         .catch(error => console.error(error));
 }
@@ -21,14 +19,16 @@ function displayMovies(movies) {
 
     movies.forEach(movie => {
         let movieCard = document.createElement('div');
-        movieCard.classList.add('col-md-3', 'mb-3');
+        movieCard.classList.add('flip', 'flip-vertical');
         movieCard.innerHTML = `
             <div class="card">
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}">   
-                <div class="card-body">
-                    <h5 class="card-title">${movie.title}</h5>
-                    <p class="card-text">${movie.overview}</p>
-                    <a href="#" class="btn btn-primary">Details</a>
+                <div class="front">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}">  
+                </div> 
+                <div class="back">
+                    <h2>${movie.title}</h2>
+                    <p>${movie.overview}</p>
+                    <p>${movie.vote_average}</p>
                 </div>
             </div>  
         `;
