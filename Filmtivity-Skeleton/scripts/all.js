8b9ea13298c774b26d82ea99d4220a8323fd48b4
Mariyan Yordanov <marsrewq@gmail.com>
2025-01-23 05:21:54 +0200

    rename project

--- Filmtivity-Skeleton/scripts/all.js
+++ Filmtivity-Skeleton/scripts/all.js
@@ -1,8 +1,31 @@
 function fetchTopTenMovies() {
-
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`)
+        .then(response => response.json())
+        .then(data => {
+            const results = data.results;
+            displayMovies(results);
+        })
+        .catch(error => console.error(error));
 }
 
 function displayMovies(movies) {
+    let movieContainer = document.getElementById('movie-container');
+    movieContainer.innerHTML = '';
 
+    movies.forEach(movie => {
+        var movieElement = ` 
+            <div class="flip flip-vertical">
+                <div class="front">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                </div>
+                <div class="back">
+                    <h2>${movie.title}</h2>
+                    <p>Description:${movie.overview}</p>
+                    <p>${movie.vote_average}</p>
+                </div>
+            </div>
+        `;
+        movieContainer += movieElement;
+    });
 }
 
--- Filmtivity-Skeleton/scripts/search.js
+++ Filmtivity-Skeleton/scripts/search.js
@@ -1,6 +1,8 @@
 const API_KEY = '25afacdd7d9acf12478bb0c74e5d129a';
 
-document.addEventListener(`DOMContentLoaded`, function () {
+document.addEventListener(`DOMContentLoaded`, searchMovie);
+
+function searchMovie() {
     document.getElementById(`search-btn`).addEventListener(`click`, function (e) {
         e.preventDefault();
         const title = document.getElementById(`search`).value;
@@ -13,18 +15,18 @@ document.addEventListener(`DOMContentLoaded`, function () {
             document.getElementById(`details-page`).style.display = `none`;
         }
     });
-});
+}
 
 function fetchMovie(title) {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`)
         .then(response => response.json())
         .then(data => {
             const results = data.results[0];
-            document.getElementById(`title`).textContent = results.title;
-            document.getElementById(`release_date`).textContent = results.release_date;
-            document.getElementById(`overview`).textContent = results.overview;
-            document.getElementById(`vote_average`).textContent = results.vote_average;
-            document.getElementById(`poster`).src = `https://image.tmdb.org/t/p/w500` + results.poster_path;
+            document.getElementById('title').textContent = results.title;
+            document.getElementById('release_date').textContent = results.release_date;
+            document.getElementById('overview').textContent = results.overview;
+            document.getElementById('vote_average').textContent = results.vote_average;
+            document.getElementById('poster').src = 'https://image.tmdb.org/t/p/w500' + results.poster_path;
         })
         .catch(error => console.error(error));
 }
